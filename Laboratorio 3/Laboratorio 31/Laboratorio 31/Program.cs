9673a836c931dcb71ed6dab95f46aa1ba9a0216a
Santiago Ospina <santyoshd@gmail.com>
2025-09-16 20:42:27 -0500

    Lab 31 completo

---
+++ Laboratorio 3/Laboratorio 31/Laboratorio 31/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int primerNumero, segundoNumero;
+
+        Console.WriteLine("Introduce el primer número: ");
+        primerNumero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo número: ");
+        segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+        // Crear instancia de la clase
+        CalculosMatematicos ca = new CalculosMatematicos();
+
+        Console.WriteLine("La operación de ({0}+{1})*({0}-{1}) es igual a {2}", primerNumero, segundoNumero, ca.Calculo(primerNumero, segundoNumero));
+
+    }
+
+    public class CalculosMatematicos
+    {
+        public int Calculo(int a, int b)
+        {
+            return (a+b)*(a-b);
+        }
+
+    }
+}
