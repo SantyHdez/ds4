c80e5d153242dd6ed2b520d5f4193b96f967d3df
Santiago Ospina <santyoshd@gmail.com>
2025-09-16 20:59:49 -0500

    Lab 32 completo

---
+++ Laboratorio 3/Laboratorio 32/Laboratorio 32/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int radio;
+
+        Console.WriteLine("Bienvenido al programa para calcular area de un circulo");
+
+        Console.WriteLine("introduce el radio del circulo");
+        radio = Convert.ToInt32(Console.ReadLine());
+
+        CalculosMatematicos ca = new CalculosMatematicos();
+
+        Console.WriteLine("El area del circulo es: " + ca.AreaCirculo(radio)); 
+
+ 
+    }
+
+    public class CalculosMatematicos
+    {
+        public int Calculo(int a, int b)
+        {
+            return (a + b) * (a - b);
+        }
+
+        public double AreaCirculo(int a)
+        {
+            double pi = 3.14159;
+            return pi * (a * a);
+        }
+
+    }
+}
--- Laboratorio 3/Laboratorio 32/Laboratorio 32/obj/Debug/net8.0/Laboratorio 32.AssemblyInfo.cs
+++ Laboratorio 3/Laboratorio 32/Laboratorio 32/obj/Debug/net8.0/Laboratorio 32.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 31")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 32")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9673a836c931dcb71ed6dab95f46aa1ba9a0216a")]
-[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 31")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 31")]
+[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 32")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 32")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
