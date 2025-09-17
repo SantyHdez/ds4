9633cdd5ea8fd586e9eba6beab63d5e737a703b3
Santiago Ospina <santyoshd@gmail.com>
2025-09-17 18:52:32 -0500

    lab 41 completo

--- Laboratorio 4/Laboratorio 41/Laboratorio 41/obj/Debug/net8.0/Laboratorio 41.AssemblyInfo.cs
+++ Laboratorio 4/Laboratorio 41/Laboratorio 41/obj/Debug/net8.0/Laboratorio 41.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 32")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 41")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c31166d6af99d43a777aa47e1c2e86820e6761")]
-[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 32")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 32")]
+[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 41")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 41")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
