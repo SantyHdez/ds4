06c31166d6af99d43a777aa47e1c2e86820e6761
Santiago Ospina <santyoshd@gmail.com>
2025-09-16 21:06:41 -0500

    Lab 33 completo

---
+++ Laboratorio 3/Laboratorio 33/Laboratorio 33/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int altura, Base;
+
+        Console.WriteLine("Bienvenido al programa para calcular perimetro de un rectangulo");
+
+        Console.WriteLine("introduce la base:");
+        Base = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("introduce la altura:");
+        altura = Convert.ToInt32(Console.ReadLine());
+
+        CalculosMatematicos ca = new CalculosMatematicos();
+
+        Console.WriteLine("El perimetro del rectangunlo es: " + ca.PerimetroRec(altura,Base));
+
+
+    }
+
+    public class CalculosMatematicos
+    {
+        public int Calculo(int a, int b)
+        {
+            return (a + b) * (a - b);
+        }
+
+        public double AreaCirculo(int a)
+        {
+            double pi = 3.14159;
+            return pi * (a * a);
+        }
+
+        public int PerimetroRec(int a, int b)
+        {
+            return (2*a)+(2*b);
+
+        }
+
+
+
+    }
+}
