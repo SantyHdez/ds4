189116797e6941849cdbe8f5be149c2335e25b6b
Santiago Ospina <santyoshd@gmail.com>
2025-09-17 19:03:17 -0500

    lab 42 completo

---
+++ Laboratorio 4/Laboratorio 42/Laboratorio 42/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+// Programa que permita obtener la factorial de un numero 
+// ingresado por el teclado
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int fac = 1, n;
+        string linea;
+        Console.WriteLine("Ingrese un numero entero: ");
+        linea = Console.ReadLine();
+        n = int.Parse(linea);
+        for (int i = 1; i <= n; i++)
+        {
+            fac = fac * i;
+
+
+        }
+
+        Console.WriteLine("La factorial es: " + fac);
+        Console.ReadKey();
+    }
+
+}
