907eb18a25ba0411d5ffb2c5575c304b3c056092
Santiago Ospina <santyoshd@gmail.com>
2025-09-17 19:51:15 -0500

    Laboratorio 45 completo

---
+++ Laboratorio 4/Laboratorio 45/Laboratorio 45/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+//multi-inicializacion de variables
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+
+        // Declaracion de variables
+
+        int edad1, edad2, edad3, edad4, edad5 = 31;
+
+        //inicializacion de variables
+        edad1=edad2=edad3=edad4=32;
+
+        Console.WriteLine("{0}, {1}, {2}, {3}, {4}", edad1, edad2, edad3, edad4, edad5);
+
+    }
+}
