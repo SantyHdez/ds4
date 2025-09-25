89623f6cd81d4c79b630306ba146810a1f173c3a
Santiago Ospina <santyoshd@gmail.com>
2025-09-24 19:59:23 -0500

    lab 71 completo

---
+++ Laboratorio 7/Laboratorio 71/Laboratorio 71/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        banco banco1 = new banco();
+        banco1.Operar();
+        banco1.DepositosTotales();
+        Console.ReadKey();
+    }
+}
---
+++ Laboratorio 7/Laboratorio 71/Laboratorio 71/banco.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class banco
+{
+    private cliente cliente1, cliente2, cliente3;
+
+    public banco()
+    {
+        cliente1 = new cliente("Yhonas");
+        cliente2 = new cliente("Ana");
+        cliente3 = new cliente("Pedro");
+    }
+
+    public void Operar()
+    {
+        cliente1.Depositar(100);
+        cliente2.Depositar(150);
+        cliente3.Depositar(200);
+        cliente3.Extraer(150);
+    }
+
+    public void DepositosTotales()
+    {
+        int t = cliente1.RetornarMonto() +
+            cliente2.RetornarMonto() +
+            cliente3.RetornarMonto();
+
+        Console.WriteLine("El total de dinero en el banco es: " + t);
+        cliente1.Imprimir();
+        cliente2.Imprimir();
+        cliente3.Imprimir();
+    }
+}
