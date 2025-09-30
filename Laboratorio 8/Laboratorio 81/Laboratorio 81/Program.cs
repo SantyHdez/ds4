3b3607947e6786a8be35043ec0d31887b90b49ff
Santiago Ospina <santyoshd@gmail.com>
2025-09-30 18:27:59 -0500

    lab 81 completo

---
+++ Laboratorio 8/Laboratorio 81/Laboratorio 81/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    public static void Main()
+    {
+        trabajador p = new trabajador("Josan", 22, "77588260-Z", 100000);
+        Console.WriteLine("Nombre=" + p.Nombre);
+        Console.WriteLine("Edad=" + p.Edad);
+        Console.WriteLine("NIF=" + p.NIF);
+        Console.WriteLine("Sueldo=" + p.Sueldo);
+        Console.ReadKey();
+    }
+}
