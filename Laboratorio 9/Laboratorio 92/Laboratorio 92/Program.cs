ce1eec7bf1114456c7dfa786e62bff879e06df0e
Santiago Ospina <santyoshd@gmail.com>
2025-10-01 08:24:59 -0500

    lab 92 completo

---
+++ Laboratorio 9/Laboratorio 92/Laboratorio 92/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Números pares o divisibles entre 3 del 1 al 100:");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+
