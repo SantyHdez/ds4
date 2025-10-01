a0a828103f426ad3afb52821bbad4407169dea72
Santiago Ospina <santyoshd@gmail.com>
2025-09-30 19:01:07 -0500

    lab 84 completa

---
+++ Laboratorio 8/Laboratorio 84/Laboratorio 84/Empleado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Empleado
+{
+    private string nombre;
+
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+}
