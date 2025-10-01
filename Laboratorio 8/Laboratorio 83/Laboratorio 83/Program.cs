2cb629f4cb6728bbbadba798937f4db5eb8976d5
Santiago Ospina <santyoshd@gmail.com>
2025-09-30 19:24:18 -0500

    lab 87 completo

--- Laboratorio 8/Laboratorio 83/Laboratorio 83/Program.cs
+++ Laboratorio 8/Laboratorio 83/Laboratorio 83/Program.cs
@@ -2,6 +2,22 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Corrio la aplicación");
+        Console.WriteLine(Suma(1.0, 2.2));
     }
+
+    static int Suma(int x, int y)
+    {
+        return x + y;
+    }
+
+    static double Suma(double x, double y)
+    {
+        return x + y;
+    }
+
+    static long Suma(long x, long y)
+    {
+        return x + y;
+    }
+
 }
