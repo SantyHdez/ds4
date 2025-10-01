f59bc6f79658d9078d9e6dbcd5b9a608386862e6
Santiago Ospina <santyoshd@gmail.com>
2025-09-30 19:07:02 -0500

    lab 85 completo

---
+++ Laboratorio 8/Laboratorio 85/Laboratorio 85/Program.cs
@@ -0,0 +1,9 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Coordenadas misCoords = new Coordenadas(10, 15);
+        misCoords.VerCoordenadas();
+    }
+}
