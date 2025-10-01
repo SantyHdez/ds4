57e36709a0f64ab5dbe5050a3194bc0691e6eff0
Santiago Ospina <santyoshd@gmail.com>
2025-10-01 08:27:11 -0500

    lab 93 completo

---
+++ Laboratorio 9/Laboratorio 93/Laboratorio 93/Program.cs
@@ -0,0 +1,28 @@
+﻿
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Ingrese el lado 1: ");
+            int lado1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el lado 2: ");
+            int lado2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el lado 3: ");
+            int lado3 = Convert.ToInt32(Console.ReadLine());
+
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                Console.WriteLine("El triángulo es Equilátero");
+            }
+            else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                Console.WriteLine("El triángulo es Isósceles");
+            }
+            else
+            {
+                Console.WriteLine("El triángulo es Escaleno");
+            }
+        }
+    }
