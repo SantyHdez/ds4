ed97b4481691657cb80cd38212be65375ca3aa5f
Santiago Ospina <santyoshd@gmail.com>
2025-10-01 08:43:06 -0500

    lab 94 completo

---
+++ Laboratorio 9/Laboratorio 94/Laboratorio 94/Aleatorios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Aleatorios
+{
+    private Random random = new Random();
+
+    public int GenerarNumero(int min, int max)
+    {
+        return random.Next(min, max + 1);
+    }
+    public int[] GenerarArreglo(int cantidad, int min, int max)
+    {
+        int[] arreglo = new int[cantidad];
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            arreglo[i] = random.Next(min, max + 1);
+        }
+
+        return arreglo;
+    }
+}
---
+++ Laboratorio 9/Laboratorio 94/Laboratorio 94/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Aleatorios aleatorios = new Aleatorios();
+
+            int numero = aleatorios.GenerarNumero(1, 10);
+            Console.WriteLine("Número generado entre 1 y 10: " + numero);
+
+            int[] arreglo = aleatorios.GenerarArreglo(5, 1, 20);
+            Console.WriteLine("\nArreglo generado:");
+            foreach (int n in arreglo)
+            {
+                Console.Write(n + " ");
+            }
+        }
+    }
