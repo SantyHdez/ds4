4115e55679900601bf0cbff97d62f19fab23fcc2
Santiago Ospina <santyoshd@gmail.com>
2025-09-30 19:57:48 -0500

    lab 91 completo

---
+++ Laboratorio 9/Laboratorio 91/Laboratorio 91/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Ingrese el precio del producto: ");
+            double precio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese la forma de pago (efectivo/tarjeta): ");
+            string formaPago = Console.ReadLine();
+
+            string numeroCuenta = "";
+            if (formaPago.ToLower() == "tarjeta")
+            {
+                Console.Write("Ingrese el número de cuenta (16 dígitos): ");
+                numeroCuenta = Console.ReadLine();
+            }
+
+        }
+    }
+
